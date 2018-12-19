47d24ab547a67b992fe561ea0814b64a3ca16e99
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2018-12-19 12:36:38 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,9 @@
+print('-'*65)
+print('Iz iT BrIcK BoT?:')
+print()
+weather = input('Temperature in Fahrenheit:')
+if weather <= '32':
+	print('Itz bRiCk, wear a jacket dummy.')
+else:
+	print('YoU aII')
+print('-'*65)
---
+++ problem2.py
@@ -0,0 +1,12 @@
+print('-'*65)
+print('Narrative Bot:')
+print()
+call = input('Student Name:')
+number = input('Grade:')
+print()
+print('Narrative:')
+if number >= '65':
+	print('Yay congrats you passed this class which is impossible to fail. So it is not like it is a great thing but yay. I guess...Keep doing what you are doing.')
+else:
+	print('So you failed...This is not good. You have mandatory tutoring. You have contiued to miss many of my classes. You will not be able to graduate without the credits frm my class.')
+print('-'*65)
